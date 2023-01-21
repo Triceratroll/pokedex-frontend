d6ea3b04085d0e37f5fc87a735c16f1b7d673243
Triceratroll <adrianmf06@gmail.com>
2023-01-21 19:24:32 +0100

    Mod basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import PokemonList from "./PokemonList";
+import PokemonGrid from "./PokemonGrid";
+import Navbar from "./Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PokemonCard from "./PokemonCard";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState([]);
+  const [gridView, setiew] = useState();
+
+  function parse(results) {
+    const pokemon_list = results.map(({ url, name, id }) => ({
+      url,
+      name,
+      id: url.match(/pokemon\/(\d+)\//)[1],
+    }));
+    return pokemon_list;
+  }
+
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=50&offset=0.", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => setList(parse(data.results))) // set
+    .catch((error) => console.error(error));
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<PokemonGrid list={list} />}></Route>
+          <Route
+            exact
+            path="/list"
+            element={<PokemonList list={list} />}
+          ></Route>
+          <Route
+            exact
+            path="/fav"
+            element={<PokemonGrid list={list} />} // only fav pokemons
+          ></Route>
+          <Route
+            path="/pokemon/:name"
+            element={<PokemonCard list={list} />} // only fav pokemons
+          ></Route>
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
