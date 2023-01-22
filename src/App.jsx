0dbd0eb6a056c3ad226913a7c6f69c8e34e096e3
Triceratroll <adrianmf06@gmail.com>
2023-01-22 23:53:26 +0100

    Fix intial loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import PokemonList from "./PokemonList";
 import PokemonGrid from "./PokemonGrid";
 import Navbar from "./Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PokemonCard from "./PokemonCard";
+import PokemonFavs from "./PokemonFavs";
+import LoadingSpinner from "./LoadingSpinner";
 
 function App() {
   const [list, setList] = useState([]);
-  const [gridView, setiew] = useState();
+  const [isLoading, setLoading] = useState(true);
 
   function parse(results) {
     const pokemon_list = results.map(({ url, name, id }) => ({
@@ -19,40 +21,49 @@ function App() {
     return pokemon_list;
   }
 
-  fetch("https://pokeapi.co/api/v2/pokemon?limit=50&offset=0.", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => setList(parse(data.results))) // set
-    .catch((error) => console.error(error));
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=50&offset=0", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setList(parse(data.results));
+        setLoading(false);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
-  return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<PokemonGrid list={list} />}></Route>
-          <Route
-            exact
-            path="/list"
-            element={<PokemonList list={list} />}
-          ></Route>
-          <Route
-            exact
-            path="/fav"
-            element={<PokemonGrid list={list} />} // only fav pokemons
-          ></Route>
-          <Route
-            path="/pokemon/:name"
-            element={<PokemonCard list={list} />}
-          ></Route>
-        </Routes>
-      </Router>
-    </div>
-  );
+  if (isLoading) {
+    return <LoadingSpinner></LoadingSpinner>;
+  } else {
+    return (
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<PokemonGrid list={list} />}></Route>
+            <Route
+              exact
+              path="/list"
+              element={<PokemonList list={list} />}
+            ></Route>
+            <Route
+              exact
+              path="/fav"
+              element={<PokemonFavs list={list} />} // only fav pokemons
+            ></Route>
+            <Route
+              path="/pokemon/:name"
+              element={<PokemonCard list={list} />}
+            ></Route>
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
