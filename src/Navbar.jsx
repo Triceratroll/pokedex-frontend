3350e88149335d19865f665dc8838c5451b7a09a
Triceratroll <adrianmf06@gmail.com>
2023-01-25 03:45:40 +0100

    Mod change li for button & Link for NavLink

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,24 +1,25 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import "./App.css";
 
 function Navbar() {
   return (
     <nav>
-      <ul className="flex">
-        <li className="m-24">
-          <Link to="/" className="text-5xl ">
-            Grid
-          </Link>
-        </li>
-        <li className="m-24">
-          <Link to="/list" className="text-5xl ">
-            List
-          </Link>
-        </li>
-        <li className="m-24">
-          <Link to="/fav" className="text-5xl ">
-            Favs
-          </Link>
-        </li>
+      <ul className="md:flex justify-center">
+        <button className="w-44 border-hidden m-5">
+          <NavLink to="/" className="text-2xl">
+            Grid view
+          </NavLink>
+        </button>
+        <button className="w-44 border-hidden m-5">
+          <NavLink to="/list" className="text-2xl">
+            List view
+          </NavLink>
+        </button>
+        <button className="w-44 border-hidden m-5">
+          <NavLink to="/fav" className="text-2xl">
+            Favorites
+          </NavLink>
+        </button>
       </ul>
     </nav>
   );
