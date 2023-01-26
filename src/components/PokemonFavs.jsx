eeca592ffedaf6ab593af7b91c272b28570f2128
Triceratroll <adrianmf06@gmail.com>
2023-01-27 00:17:16 +0100

    Mod component to external component

--- src/components/PokemonFavs.jsx
+++ src/components/PokemonFavs.jsx
@@ -1,11 +1,10 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import { compareArrays } from "../tools/compareArrays.js";
 import { toggleArrayElement } from "../tools/toggleArrays";
 import { usePaginationFetch } from "../hooks/usePaginationfetch";
-import star_solid from "../assets/star_solid.svg";
 import Navbar from "./II_level/Navbar";
 import Loading from "./II_level/Loading";
+import PokemonCard from "./II_level/PokemonCard";
 import "../App.css";
 
 const getUrl = (page, pageSize) =>
@@ -56,34 +55,13 @@ function PokemonFavs({}) {
         {modifiedList.map(
           ({ id, name, match }) =>
             match && (
-              <Link
-                className="Pokemon"
-                id={id}
+              <PokemonCard
                 key={id}
-                to={`/pokemon/${name}`}
-              >
-                <div className="relative w-48 h-80 flex items-center justify-center bg-indigo-300 hover:-mt-2 border-8 border-yellow-300 shadow-yellow rounded m-4">
-                  <div className="absolute top-0 left-0 text-black text-xl ml-1 mt-1">
-                    {"#" + id}
-                  </div>
-                  <div className="absolute top-0 right-0">
-                    <img
-                      onClick={handleFavorite}
-                      className="w-6  mr-1 mt-2"
-                      src={star_solid}
-                      alt=""
-                    />
-                  </div>
-                  <img
-                    className="inline-block rounded"
-                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
-                    alt=""
-                  />
-                  <div className="absolute text-black text-xl bottom-0 inset-x-0">
-                    {name.charAt(0).toUpperCase() + name.slice(1)}
-                  </div>
-                </div>
-              </Link>
+                id={id}
+                name={name}
+                match={match}
+                handleFavorite={handleFavorite}
+              ></PokemonCard>
             )
         )}
       </div>
